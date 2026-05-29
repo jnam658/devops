8093d34c03170dbb2a0c0eb11c7db679934f72d9
jnam658 <jnam658@gmail.com>
2026-05-29 10:57:25 +0900

    1

---
+++ test1/src/test1/Calculator.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public String add(String a, String b) {
+		return a + b;
+	}
+}
---
+++ test1/src/test2/StudentTest.java
@@ -0,0 +1,52 @@
+package test2;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StudentTest {
+	static Student std;
+	@BeforeAll
+	static void setUp() throws Exception {
+		std = new Student();
+	}
+
+	@Test
+	@Order(1)
+	void testAddStudent() {
+		std.addStudent("홍길동");
+        assertTrue(std.hasStudent("홍길동"));
+	}
+	
+	@Test
+	@Order(2)
+	void testStudentAlreadyExist() {
+		assertThrows(
+		        IllegalArgumentException.class,
+		        () -> std.addStudent("홍길동")
+		    );
+	}
+
+	@Test
+	@Order(3)
+	void testRemoveStudent() {
+		std.removeStudent("홍길동");
+		assertFalse(std.hasStudent("홍길동"));
+	}
+	
+	@Test
+	@Order(4)
+	void testDeleteInvaidStudent() {
+		assertThrows(
+				IllegalArgumentException.class,
+				()-> std.removeStudent("홍길동")
+			);
+	}
+	
+	
+
+}
